65714264295a97f5da6929274121239525514ddf
Leonid <Leoon111@gmail.com>
2021-04-01 09:56:09 +0300

    Создание БД, проверка наличие таблицы, создание заголовков

---
+++ Test/DataBase/MySqlCon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Test.DataBase
+{
+    public class MySqlCon
+    {
+        public MySqlConnection GetConntction()
+        {
+            string connStr = "server=localhost;user=root;database=test;port=3306;password=1q2w3e4r5t";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return conn;
+        }
+    }
+}
